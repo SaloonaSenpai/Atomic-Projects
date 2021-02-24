9510f4ca136475302665cbc51c41721d59ce7bd1
Prolix Senpai <46688597+ProlixSenpai@users.noreply.github.com>
2021-02-24 23:53:29 +0400

    Update 0.1
    - Replaced > < { to the correct term for it
    - resized the url bar
    - attempted to make a history bar
    - added stop to the nav bar

--- browser.py
+++ browser.py
@@ -29,36 +29,54 @@ class Mainwindow(QMainWindow):
         navbar = QToolBar()
         #adding a navbar
         self.addToolBar(navbar)
+    
+    
         
         #adding some tool bars
         
         #back button 
-        back_btn = QAction('<<<',self)    #declaring the back btn using action 
+        back_btn = QAction('back',self)    #declaring the back btn using action 
         back_btn.triggered.connect(self.browser.back)   #when its triggered connect to the browser and go back [type of connect is back]
         navbar.addAction(back_btn)  #adding it to the navbar 
         
+        
         #forward 
-        forward_btn = QAction('>>>',self) #declaring the foward btn using action 
+        forward_btn = QAction('forward',self) #declaring the foward btn using action 
         forward_btn.triggered.connect(self.browser.forward) #when its triggered connect to the browser and go forward [type of connect is forward]
         navbar.addAction(forward_btn)
         
         #reload 
-        reload_btn = QAction("{{}}",self) #declaring the reload btn using action 
+        reload_btn = QAction("reload",self) #declaring the reload btn using action 
         reload_btn.triggered.connect(self.browser.reload)   #connect reload when tiggered
         navbar.addAction(reload_btn)    #add it to the navbar
         
-        #Home 
-        home_btn = QAction("Home",self)
-        home_btn.triggered.connect(self.navi_home)  #when clicking on this home button, a method would be running
-        navbar.addAction(home_btn)
+        #stop 
+        stop_btn = QAction("X",self)
+        stop_btn.triggered.connect(self.browser.stop)
+        navbar.addAction(stop_btn)
+        
 
         #nav url_bar 
         self.url_bar = QLineEdit()   #text line to write in
+        self.url_bar.setFixedSize(500,20)   #resizing the bar
+        self.url_bar.setStyleSheet("QLineEdit { border-radius: 5px; }")    #adding radius to the bar
         self.url_bar.returnPressed.connect(self.navi_to_url)    #when return is pressed, connect to specific thing that would be a method
         navbar.addWidget(self.url_bar)  #adding the url bar to the navbar
         
         #if url has changed you need to update the URL BAR
         self.browser.urlChanged.connect(self.update_url_bar)
+
+        #Home 
+        home_btn = QAction("Home",self)
+        home_btn.triggered.connect(self.navi_home)  #when clicking on this home button, a method would be running
+        navbar.addAction(home_btn)
+        
+        #history 
+        #history = QWebEngineHistory()
+        #home_btn.triggered.connect(self.browser.history)
+        #navbar.addWidget(self.history_btn) 
+
+        
         
     def navi_home(self):
         self.browser.setUrl(QUrl("https://duckduckgo.com")) #set the url back to the original
@@ -81,8 +99,7 @@ class Mainwindow(QMainWindow):
         #takes the parameter and sets it as the text in the url bar
         
     
-    
-    
+
     
     
     
